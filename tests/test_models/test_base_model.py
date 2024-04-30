db89dde60b3b5ce948c5e42dde97e120dfc7d903
Anthony Gbobo <psalmseins@gmail.com>
2024-04-29 17:03:51 -0700

    Tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -148,12 +148,12 @@ class TestBaseModel(unittest.TestCase):
     def test_save(self, mock_storage):
         """Test that save method updates `updated_at` and calls
         `storage.save`"""
-        inst = BaseModel()
-        old_created_at = inst.created_at
-        old_updated_at = inst.updated_at
-        inst.save()
-        new_created_at = inst.created_at
-        new_updated_at = inst.updated_at
+        obj = BaseModel()
+        old_created_at = obj.created_at
+        old_updated_at = obj.updated_at
+        obj.save()
+        new_created_at = obj.created_at
+        new_updated_at = obj.updated_at
         self.assertNotEqual(old_updated_at, new_updated_at)
         self.assertEqual(old_created_at, new_created_at)
         self.assertTrue(mock_storage.new.called)
