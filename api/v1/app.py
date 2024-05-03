cad478d2da575a8f9857153907693f87ba1530ba
Anthony Gbobo <psalmseins@gmail.com>
2024-05-02 17:17:12 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,19 +10,6 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
-
-
-@app.teardown_appcontext
-def downtear(self):
-    '''Status of your API'''
-    storage.close()
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
-    return jsonify('error='Not found'), 404
 
 
 if __name__ == '__main__':
