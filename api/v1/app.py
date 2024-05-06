7653706dea3b3f5d7976bc8fe9df3d14cbdbec3f
Anthony Gbobo <psalmseins@gmail.com>
2024-05-05 18:40:08 -0700

    correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,12 +12,6 @@ app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
-    return jsonify('error='Not found'), 404
-
-
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST')
     port = getenv('HBNB_API_PORT')
